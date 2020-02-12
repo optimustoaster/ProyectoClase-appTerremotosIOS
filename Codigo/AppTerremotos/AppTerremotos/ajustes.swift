a13763609d6a531229d70322360f3f69508c8d27
Optimus <mikel.gonzalez@ikasle.egibide.org>
2020-02-12 21:08:46 +0100

    1.0

---
+++ Codigo/AppTerremotos/AppTerremotos/ajustes.swift
@@ -0,0 +1,9 @@
+//
+//  ajustes.swift
+//  AppTerremotos
+//
+//  Created by  on 07/02/2020.
+//  Copyright © 2020 none.none. All rights reserved.
+//
+
+import Foundation
---
+++ Codigo/AppTerremotos/AppTerremotos/modeloTerremoto.swift
@@ -0,0 +1,9 @@
+//
+//  modeloTerremoto.swift
+//  AppTerremotos
+//
+//  Created by  on 05/02/2020.
+//  Copyright © 2020 none.none. All rights reserved.
+//
+
+import Foundation
