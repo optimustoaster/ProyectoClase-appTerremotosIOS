37673cab41ced3bdb6d4611796f88bfbf0a01494
Optimus <mikel.gonzalez@ikasle.egibide.org>
2020-02-14 21:13:23 +0100

    1.1

---
+++ Codigo/AppTerremotosV1.1/AppTerremotos/MapaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapaViewController.swift
+//  AppTerremotos
+//
+//  Created by  on 14/02/2020.
+//  Copyright © 2020 none.none. All rights reserved.
+//
+
+import UIKit
+
+class MapaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Codigo/AppTerremotosV1.1/AppTerremotos/mapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  mapViewController.swift
+//  AppTerremotos
+//
+//  Created by  on 14/02/2020.
+//  Copyright © 2020 none.none. All rights reserved.
+//
+
+import SwiftUI
+
+struct mapViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct mapViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        mapViewController()
+    }
+}
